5425d667dd401bccd5ef4741b8d6fc92e995ee4f
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-04-01 18:37:46 +0500

    Add logo and search in sidebar+grid layout sidebar

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -2,13 +2,19 @@ import cn from 'classnames'
 import { DetailedHTMLProps, HTMLAttributes } from 'react'
 import styles from './Sidebar.module.scss'
 import { Menu } from '../Menu/Menu'
+import Logo from '../logo.svg'
+
 
 
 interface SidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
 
-export const Sidebar = ({ ...props }: SidebarProps) => {
+export const Sidebar = ({ className, ...props }: SidebarProps) => {
 	return (
-		<div {...props}>
+		<div className={cn(className, styles.sidebar)} {...props} >
+			<Logo className={styles.sidebarLogo} />
+			<div>
+				Search
+			</div>
 			<Menu />
 		</div>
 	)
