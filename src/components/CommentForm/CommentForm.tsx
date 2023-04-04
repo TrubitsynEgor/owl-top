efc24986a8283cd90ea57b5ef8bd39533344c4fd
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-04-04 17:59:08 +0500

    add UseForm +onSubmit +Rating state access(all info on submit add in obj data)

--- src/components/CommentForm/CommentForm.tsx
+++ src/components/CommentForm/CommentForm.tsx
@@ -5,36 +5,58 @@ import { Rating } from '../Rating/Rating'
 import { TextArea } from '../UI/TextArea/TextArea'
 import { Button } from '../UI/Button/Button'
 import CloseIcon from './close.svg'
+import { useForm, Controller } from 'react-hook-form'
 
 
 interface CommentFormProps extends DetailedHTMLProps<FormHTMLAttributes<HTMLFormElement>, HTMLFormElement> {
 	productId: string
 }
 
+export interface ICommentForm {
+	name: string
+	title: string
+	description: string
+	rating: number
+}
+
 const CommentForm = ({ productId }: CommentFormProps) => {
-	const [rating, setRating] = useState<number>(0)
+
+	const { register, control, handleSubmit, } = useForm<ICommentForm>()
+
+
+	const onSubmit = (data: ICommentForm) => {
+		console.log(data);
+
+	}
 
 	return (
-		<>
-			<form action="#" className={styles.commentForm}>
-				<div className={styles.top}>
-					<Input className={styles.input} placeholder='Имя' name='name' />
-					<Input className={styles.input} placeholder='Заголовок отзыва' />
-					<span className={styles.rating}>Оценка:<Rating rating={rating} isEditable setRating={setRating} /></span>
-				</div>
-				<TextArea placeholder='Текст отзыва' className={styles.textArea} />
-				<div className={styles.bot}>
-					<Button appearance='primary' className={styles.btn}>Отправить</Button>
-					<span className={styles.caption}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
-				</div>
-			</form>
+		<form onSubmit={handleSubmit(onSubmit)} action="#" className={styles.commentForm}>
+			<div className={styles.top}>
+				<Input {...register('name')} className={styles.input} placeholder='Имя' name='name' />
+				<Input {...register('title')} className={styles.input} placeholder='Заголовок отзыва' />
+
+				<span className={styles.rating}>Оценка:
+					<Controller control={control} name='rating' render={({ field }) => (
+						<Rating rating={field.value} isEditable setRating={field.onChange} />
+					)
+					} />
+				</span>
+
+			</div>
+			<TextArea {...register('description')} placeholder='Текст отзыва' className={styles.textArea} />
+			<div className={styles.bot}>
+				<Button appearance='primary' className={styles.btn}>Отправить</Button>
+				<span className={styles.caption}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
+			</div>
 
 			<div className={styles.success}>
 				<div className={styles.successTitle}>Ваш комментарий отправлен!</div>
 				<div className={styles.successCaption}>Спасибо что отправили комментарий, он будет опубликован после проверки</div>
 				<CloseIcon className={styles.successIcon} />
 			</div>
-		</>
+		</form>
+
+
 	)
 }
 
